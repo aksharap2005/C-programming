b41f4400f9ca01d2cb1967b5838e7ebd4879389b
aksharap2005 <131359971+aksharap2005@users.noreply.github.com>
2023-05-11 18:58:13 +0530

    Add files via upload

---
+++ increement.decreement.c
@@ -0,0 +1,36 @@
+//PRE INCREEMENT
+//#include<stdio.h>
+//int main()
+//{int a;
+//printf("enter a number");
+//scanf("%d",&a);
+//printf("increement is %d",++a);
+//}
+
+//POST INCREEMENT
+//#include<stdio.h>
+//int main()
+//{int b;
+//printf("enter a number");
+//scanf("%d",&b);
+//printf("increement is %d",b++);
+//}
+
+//PRE DECREEMENT
+//#include<stdio.h>
+//int main()
+//{int c;
+//printf("enter a number");
+//scanf("%d",&c);
+//printf("decreement is %d",--c);
+//}
+
+//PRE DECREEMENT
+//#include<stdio.h>
+//int main()
+//{int d;
+//printf("enter a number");
+//scanf("%d",&d);
+//printf("decreement is %d",d--);
+//}
+
