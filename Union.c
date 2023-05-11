2d90b0cd40dc2278ec58f40e58daac1bee5810a8
aksharap2005 <131359971+aksharap2005@users.noreply.github.com>
2023-05-11 18:51:51 +0530

    Add files via upload

---
+++ Union.c
@@ -0,0 +1,38 @@
+//#include<stdio.h>
+//#include<string.h>
+//union employe
+//{ 
+//char name[10];
+//int rollno;
+//};
+//int main()
+//{ union employe e1,a;
+//printf("enter name and roll no");
+//scanf("%s %d",&a,&e1.rollno);
+//e1.name;
+//printf("name is %s\n",a);
+//printf("salary is %d\n",e1.rollno);
+//}
+
+#include<stdio.h>
+int main()
+{int a,b;
+printf("enter two numbers");
+scanf("%d %d",&a,&b);
+printf("enter a choice 1.add,2.sub,3.mult,4.div");
+{ choice 1: 
+{printf("addition is %d\n",a+b);
+}
+choice 2:
+	{printf("substraction is %d\n",a-b);
+	}
+choice 3:
+	{printf("multiplicaton is %d\n",a*b);
+	}
+choice 4:
+{printf("division is %d\n",a/b);
+	}	
+}
+}
+
+
