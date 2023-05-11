fc6804ef31867acc177fcd8bc02654d2c31978f4
aksharap2005 <131359971+aksharap2005@users.noreply.github.com>
2023-05-11 18:10:40 +0530

    Add files via upload

---
+++ relational.logical.operat.c
@@ -0,0 +1,60 @@
+//RELATIONAL OPERATORS
+//#include<stdio.h>
+//int main()
+//{int a,b;
+//printf("enter two numbers");
+//scanf("%d %d",&a,&b);
+//printf("both are equal %d\n",a==b);
+//printf("second number is greater %d\n",a<b);
+//printf("first number is greater %d\n",a>b);
+//}
+
+//#include<stdio.h>
+//int main()
+//{int a,b;
+//printf("enter two numbers");
+//scanf("%d %d",&a,&b);
+//printf("%d",a<b);
+//}
+
+//LOGICAL OPERATORS
+//AND 
+
+//#include<stdio.h>
+//int main()
+//{
+//int a,b;
+//printf("enter two numbers");
+//scanf("%d %d",&a,&b);
+//printf("%d",a&&b);
+//}
+
+//OR
+//#include<stdio.h>
+//int main()
+//{
+//int a,b;
+//printf("enter two numbers");
+//scanf("%d %d",&a,&b);
+//printf("%d",a||b);
+//}
+
+//XOR
+//#include<stdio.h>
+//int main()
+//{
+//int a,b;
+//printf("enter two numbers");
+//scanf("%d %d",&a,&b);
+//printf("%d",a^b);
+//}
+
+//NOT
+//#include<stdio.h>
+//int main()
+//{
+//int b;
+//printf("enter two numbers");
+//scanf("%d",&b);
+//printf("%d",!b);
+//}
