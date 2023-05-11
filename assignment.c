7f15778228fd26f6306d7020cea4ae146c554251
aksharap2005 <131359971+aksharap2005@users.noreply.github.com>
2023-05-11 18:14:29 +0530

    Add files via upload

---
+++ assignment.c
@@ -0,0 +1,84 @@
+//#include<stdio.h>
+//int main()
+//{int a;
+//printf("enter selling price");
+//scanf("%d",&a);
+//int b;
+//printf("enter cost price");
+//scanf("%d",&b);
+//if(a>b)
+//{
+//printf("profit of %d\n",a-b);
+//}
+//else
+//printf("loss of %d",b-a);}
+
+
+//#include<stdio.h>
+//int main()
+//{int a;
+//printf("enter a number");
+//scanf("%d",&a);
+//if(a%2==0)
+//printf("even");
+//else
+//{printf("odd");
+//}
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{int a;
+//a=1;
+//while(a<11)
+//{printf("%d\n",a);
+//a++;
+//}}
+
+//#include<stdio.h>
+//#include<string.h>
+//struct student
+//{ char name[40];
+//int rollno;
+//float marks;
+//};
+//int main()
+//{struct student s1,s2,s3;
+//strcpy(s1.name,"shrushti");
+//s1.rollno=1;
+//s1.marks=79.9;
+//printf("name is %s\n",s1.name);
+//printf("rollno is %d\n",s1.rollno);
+//printf("marks is %f\n",s1.marks);
+//strcpy(s2.name,"nicki");
+//s2.rollno=2;
+//s2.marks=45,8;
+//printf("name is %s\n",s2.name);
+//printf("rollno is %d\n",s2.rollno);
+//printf("marks is %f\n",s2.marks);
+//strcpy(s3.name,"kajal");
+//s3.rollno=3;
+//s3.marks=89.5;
+//printf("name is %s\n",s3.name);
+//printf("rollno is %d\n",s3.rollno);
+//printf("marks is %f\n",s3.marks);
+//
+//printf("kajal topped this exam");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ bitwise.operator.c
@@ -0,0 +1,23 @@
+//#include<stdio.h>
+//int main()
+//{int a,b;
+//printf("enter two numbers");
+//scanf("%d %d",&a,&b);
+//printf("%d",a&b);
+//}
+
+//#include<stdio.h>
+//int main()
+//{int a,b;
+//printf("enter two numbers");
+//scanf("%d %d",&a,&b);
+//printf("%d",a|b);
+//}
+
+//#include<stdio.h>
+//int main()
+//{int a,b;
+//printf("enter two numbers");
+//scanf("%d %d",&a,&b);
+//printf("%d",a^b);
+//}
