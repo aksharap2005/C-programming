ef0e2a1c5a886a744b91c7f4231f8cd019a315f9
aksharap2005 <131359971+aksharap2005@users.noreply.github.com>
2023-05-11 18:49:48 +0530

    Add files via upload

---
+++ structure.c
@@ -0,0 +1,68 @@
+//#include<stdio.h>
+//#include<string.h>
+//struct student
+//{int rollno;
+//	char name[10];
+//	float marks;};
+//int main()
+//{struct student s1,s2;
+//s1.rollno=2;
+//strcpy(s1.name,"purvi");
+//s1.marks=89.7;
+//printf("Roll no is %d\n",s1.rollno);
+//printf("name is %s\n",s1.name);
+//printf("marks are %f\n",s1.marks);
+//s2.rollno=3;
+//strcpy(s2.name,"heena");
+//s2.marks=70.4;
+//printf("Roll no is %d\n",s2.rollno);
+//printf("name is %sn",s2.name);
+//printf("marks are %f\n",s2.marks);}
+
+
+//#include<stdio.h>
+//#include<string.h>
+//struct employe
+//{ int salary;
+//char name[10];
+//int id;
+//};
+//int main()
+//{ struct employe e1,e2,e3;
+//strcpy(e1.name,"sachin");
+//e1.salary=40000;
+//e1.id=1234;
+//printf("name is %s\n",e1.name);
+//printf("salary is %d\n",e1.salary);
+//printf("id is %d\n",e1.id);
+//
+//strcpy(e2.name,"rohan");
+//e2.salary=50000;
+//e2.id=9876;
+//printf("name is %s\n",e2.name);
+//printf("salary is %d\n",e2.salary);
+//printf("id is %d\n",e2.id);
+//
+//strcpy(e3.name,"sachin");
+//e3.salary=60000;
+//e3.id=5678;
+//printf("name is %s\n",e3.name);
+//printf("salary is %d\n",e3.salary);
+//printf("id is %d\n",e3.id);}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ swap.c
@@ -0,0 +1,26 @@
+//WITH USING THIRD VARIABLE
+//#include<stdio.h>
+//int main()
+//{int a,b,c;
+//printf("enter two numbers");
+//scanf("%d %d",&a,&b);
+//printf("before swapping %d %d",a,b);
+//c=a;
+//a=b;
+//b=c;
+//printf("after swapping %d %d",a,b);
+//}
+
+//WITHOUT USING THIRD VARIABLE
+//#include<stdio.h>
+//int main()
+//{int a,b;
+//printf("enter two numbers");
+//scanf("%d %d",&a,&b);
+//printf("before sqwapping %d %d",a,b);
+//a=a+b;
+//b=a-b;
+//a=a-b;
+//printf("after swapping %d %d",a,b);
+//}
+
